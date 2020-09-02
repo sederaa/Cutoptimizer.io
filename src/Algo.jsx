760aa5618d0bb22d6a9954d7251f94b8eba6db9e
Seb <seb@dfd.co.nz>
2020-09-02 17:39:40 +1200

    Add support for buyable stock

--- src/Algo.jsx
+++ src/Algo.jsx
@@ -4,6 +4,20 @@ import clone from 'rfdc';
 export const Algo = () => {
 
   // Non-obvious solution, where all cuts are taken from 2nd stock piece. trips up most other optimizers.
+  // let originalState = {
+  //   cuts: [
+  //     { id: 0, len: 16, done: false },
+  //     { id: 1, len: 16, done: false },
+  //     { id: 2, len: 16, done: false },
+  //     { id: 3, len: 16, done: false }
+  //   ],
+  //   stock: [
+  //     { id: 0, len: 63, cuts: [] },
+  //     { id: 1, len: 64, cuts: [] }
+  //   ]
+  // };
+
+
   let originalState = {
     cuts: [
       { id: 0, len: 16, done: false },
@@ -12,12 +26,14 @@ export const Algo = () => {
       { id: 3, len: 16, done: false }
     ],
     stock: [
-      { id: 0, len: 63, cuts: [] },
-      { id: 1, len: 64, cuts: [] }
+      { id: 0, len: 15, cuts: [], price: 0 },
+      { id: 1, len: 18, cuts: [], price: 0 }
+    ],
+    buyableStock: [
+      { id: 2, len: 15, cuts: [], price: 1.23 },
+      { id: 3, len: 20, cuts: [], price: 2.34 },
     ]
   };
-
-
 
   // Pre-condition: All stock & buyablestock must have unique ids
 
@@ -28,17 +44,24 @@ export const Algo = () => {
     stateArg.stock.forEach(s => s.waste = s.len - s.used);
     stateArg.wasteTotal = stateArg.stock.reduce((accumulator, currentValue) => accumulator + currentValue.waste, 0);
     stateArg.wastePieces = stateArg.stock.reduce((accumulator, currentValue) => accumulator + (currentValue.waste > 0 ? 1 : 0), 0);
+    stateArg.cost = stateArg.stock.reduce((accumulator, currentValue) => accumulator + currentValue.price, 0);
     return stateArg;
   }
 
   const createSolution = (stateArg, stockId) => {
-    //console.group(`optimize: stockIndex = ${stockIndex}.`)
+    //console.group(`optimize: stockId = ${stockId}.`)
     let state = clone()(stateArg);
-    //console.debug(`optimize: stateArg = `, clonedState, `, stockIndex = ${stockIndex}.`);
+    //console.debug(`optimize: stateArg = `, state, `, stockId = ${stockId}.`);
     let cut = state.cuts.find(c => !c.done);
 
     //console.debug(`Cut to fit: `, cut);
     let stock = state.stock.find(s => s.id === stockId);
+    let buyableStock = state.buyableStock.find(s => s.id === stockId);
+    if (buyableStock){
+      //console.debug(`Found buyable stock ${buyableStock.id}, cloning and adding to stock...`);
+      stock = clone()(buyableStock);
+      state.stock.push(stock);
+    }
 
     state.path = state.path || "";
     state.path += ` (cut=${cut.id},stock=${stock.id})`;
@@ -46,8 +69,18 @@ export const Algo = () => {
     if (cut.len <= (stock.len - stock.cuts.reduce((accumulator, currentValue) => accumulator + currentValue.len, 0))) {
       stock.cuts.push(cut);
       cut.done = true;
+    } else if (state.buyableStock.some(bs => bs.len >= cut.len)) {
+      //console.debug(`Can't fit cut ${cut.id} into stock ${stock.id}, but there is buyable stock that would fit.`);
+  
+      for (const buyableStock of state.buyableStock.filter(bs => bs.len >= cut.len)) {
+        //console.debug(`Would call createSolution with id ${buyableStock.id}. state = `, state);
+        createSolution(state, buyableStock.id);
+      }
+  
+      return;
+  
     } else {
-      console.debug(`Can't fit cut ${cut.id} into stock ${stock.id}. Bailing out...`);
+      //console.debug(`Can't fit cut ${cut.id} into stock ${stock.id}. Bailing out...`);
       //console.groupEnd();
       return;
     }
