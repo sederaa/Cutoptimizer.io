c569954c7528e689b51229bac1d8b57e93f7e9ac
Seb <seb@dfd.co.nz>
2020-11-11 18:18:02 +1300

    Split ListMachine into separate file

--- src/List.tsx
+++ src/List.tsx
@@ -2,102 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import { useMachine } from '@xstate/react';
 import { Machine, assign } from 'xstate';
-
-enum ListStates {
-    Idle = "Idle",
-    Deleting = "Deleting",
-    Adding = "Adding",
-    UpdatingField = "UpdatingField"
-}
-
-interface ListSchema {
-    states: {
-        [ListStates.Idle]: {},
-        [ListStates.Deleting]: {},
-        [ListStates.Adding]: {},
-        [ListStates.UpdatingField]: {},
-    }
-}
-
-enum ListEvents {
-    Add = "Add",
-    Delete = "Delete",
-    UpdateField = "UpdateField"
-}
-type DeleteEvent = {
-    type: ListEvents.Delete,
-    id: number
-}
-type UpdateFieldEvent = {
-    type: ListEvents.UpdateField,
-    id: number,
-    name: keyof (Omit<ListItemData, "id">),
-    value: string
-}
-type ListEvent = { type: ListEvents.Add } | DeleteEvent | UpdateFieldEvent;
-
-interface ListItemData {
-    id: number,
-    name: string,
-    length: number,
-    quantity: number
-}
-const makeEmptyListItemData = (id: number) => ({ id, name: "", length: 0, quantity: 1 } as ListItemData);
-
-interface ListContext {
-    items: ListItemData[],
-}
-
-const ListMachine = Machine<ListContext, ListSchema, ListEvent>({
-    initial: ListStates.Idle,
-    context: {
-        items: [makeEmptyListItemData(0)]
-    },
-    states: {
-        [ListStates.Idle]: {
-            on: {
-                [ListEvents.Add]: {
-                    target: ListStates.Adding,
-                },
-                [ListEvents.Delete]: {
-                    cond: (context, event) => context.items.length > 1,
-                    target: ListStates.Deleting,
-                },
-                [ListEvents.UpdateField]: {
-                    target: ListStates.UpdatingField
-                }
-            }
-        },
-        [ListStates.UpdatingField]: {
-            entry: assign({
-                items: (context, event: UpdateFieldEvent) => [
-                    ...context.items.filter(i => i.id !== event.id),
-                    ({ ...context.items.find(i => i.id === event.id), [event.name]: event.value } as ListItemData)
-                ]
-            }),
-            always: ListStates.Idle
-        },
-        [ListStates.Deleting]: {
-            entry: [
-                assign({
-                    items: (context, event) => ([...context.items.filter(i => i.id !== (event as DeleteEvent).id)])
-                }),
-                assign({ items: (context, event) => (context.items.length > 1 ? context.items : [makeEmptyListItemData(0)]) })
-            ],
-            always: ListStates.Idle
-        },
-        [ListStates.Adding]: {
-            entry: assign({
-                items: (context, event) => {
-                    const highestExistingId = context.items.reduce((highestId, currentValue) => currentValue.id > highestId ? currentValue.id : highestId, 0);
-                    const result = [...context.items, makeEmptyListItemData(highestExistingId + 1)];
-                    return result;
-                }
-            }),
-            always: ListStates.Idle
-        },
-    }
-})
+import {ListMachine, ListEvents, ListItemData, UpdateFieldEvent} from './ListMachine';
 
 export const List = () => {
     const [state, sendEvent] = useMachine(ListMachine);
@@ -137,5 +42,3 @@ export const ListItem = ({ data, handleDeleteItem, handleUpdateField }: ListItem
         <button onClick={() => handleDeleteItem(data.id)}>x</button>
     </li>;
 }
-
-// Fix bug: if you try to backspace length or quantity fields until empty, it doesn't work
---
+++ src/ListMachine.ts
@@ -0,0 +1,97 @@
+import { Machine, assign } from 'xstate';
+
+export enum ListStates {
+    Idle = "Idle",
+    Deleting = "Deleting",
+    Adding = "Adding",
+    UpdatingField = "UpdatingField"
+}
+
+interface ListSchema {
+    states: {
+        [ListStates.Idle]: {},
+        [ListStates.Deleting]: {},
+        [ListStates.Adding]: {},
+        [ListStates.UpdatingField]: {},
+    }
+}
+
+export enum ListEvents {
+    Add = "Add",
+    Delete = "Delete",
+    UpdateField = "UpdateField"
+}
+type DeleteEvent = {
+    type: ListEvents.Delete,
+    id: number
+}
+export type UpdateFieldEvent = {
+    type: ListEvents.UpdateField,
+    id: number,
+    name: keyof (Omit<ListItemData, "id">),
+    value: string
+}
+type ListEvent = { type: ListEvents.Add } | DeleteEvent | UpdateFieldEvent;
+
+export interface ListItemData {
+    id: number,
+    name: string,
+    length: number,
+    quantity: number
+}
+const makeEmptyListItemData = (id: number) => ({ id, name: "", length: 0, quantity: 1 } as ListItemData);
+
+interface ListContext {
+    items: ListItemData[],
+}
+
+export const ListMachine = Machine<ListContext, ListSchema, ListEvent>({
+    initial: ListStates.Idle,
+    context: {
+        items: [makeEmptyListItemData(0)]
+    },
+    states: {
+        [ListStates.Idle]: {
+            on: {
+                [ListEvents.Add]: {
+                    target: ListStates.Adding,
+                },
+                [ListEvents.Delete]: {
+                    cond: (context, event) => context.items.length > 1,
+                    target: ListStates.Deleting,
+                },
+                [ListEvents.UpdateField]: {
+                    target: ListStates.UpdatingField
+                }
+            }
+        },
+        [ListStates.UpdatingField]: {
+            entry: assign({
+                items: (context, event: UpdateFieldEvent) => [
+                    ...context.items.filter(i => i.id !== event.id),
+                    ({ ...context.items.find(i => i.id === event.id), [event.name]: event.value } as ListItemData)
+                ]
+            }),
+            always: ListStates.Idle
+        },
+        [ListStates.Deleting]: {
+            entry: [
+                assign({
+                    items: (context, event) => ([...context.items.filter(i => i.id !== (event as DeleteEvent).id)])
+                }),
+                assign({ items: (context, event) => (context.items.length > 1 ? context.items : [makeEmptyListItemData(0)]) })
+            ],
+            always: ListStates.Idle
+        },
+        [ListStates.Adding]: {
+            entry: assign({
+                items: (context, event) => {
+                    const highestExistingId = context.items.reduce((highestId, currentValue) => currentValue.id > highestId ? currentValue.id : highestId, 0);
+                    const result = [...context.items, makeEmptyListItemData(highestExistingId + 1)];
+                    return result;
+                }
+            }),
+            always: ListStates.Idle
+        },
+    }
+})
