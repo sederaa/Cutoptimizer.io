84c0034b6d96c1116f9e5e35c47ed8ddc01f4057
Seb <seb@dfd.co.nz>
2022-02-10 21:58:45 +1300

    Install and configure sentry.io

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "main/components/App";
 import * as serviceWorker from "./serviceWorker";
+import * as Sentry from "@sentry/react";
+import { BrowserTracing } from "@sentry/tracing";
+
+Sentry.init({
+    dsn: "https://5a5c41d59dc745c1a6e18c4fcd58b939@o1140389.ingest.sentry.io/6197530",
+    integrations: [new BrowserTracing()],
+
+    // Set tracesSampleRate to 1.0 to capture 100%
+    // of transactions for performance monitoring.
+    // We recommend adjusting this value in production
+    tracesSampleRate: 1.0,
+});
 
 ReactDOM.render(
     <React.StrictMode>
