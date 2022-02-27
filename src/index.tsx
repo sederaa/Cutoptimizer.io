3844ac3805ab5d147376a18cea4a0bef3c655728
Seb <seb@dfd.co.nz>
2022-02-27 21:15:30 +1300

    Add analytics (getinsights.io)

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import App from "main/components/App";
 import * as serviceWorker from "./serviceWorker";
 import * as Sentry from "@sentry/react";
 import { BrowserTracing } from "@sentry/tracing";
+import { init, trackPages } from "insights-js";
 
 Sentry.init({
     dsn: "https://5a5c41d59dc745c1a6e18c4fcd58b939@o1140389.ingest.sentry.io/6197530",
@@ -14,6 +15,11 @@ Sentry.init({
     // We recommend adjusting this value in production
     tracesSampleRate: 1.0,
 });
+
+if (process.env.NODE_ENV === "production") {
+    init("USOiy_1fg8ToJIjy");
+    trackPages();
+}
 
 ReactDOM.render(
     <React.StrictMode>
